4d0de8eb5449dfa1aec55c0333d711becf29f45d
Empika1 <dravidts1@gmail.com>
2025-04-27 20:18:24 -0400

    change createModule to addModule

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     //library
-    const libMod = b.createModule(.{
+    const libMod = b.addModule("units", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
